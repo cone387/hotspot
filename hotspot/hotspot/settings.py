6fcba27d5cb375c2d56d6b1f5d4fb734488a7049
cone <1183008540@qq.com>
2019-09-03 11:21:32 +0800

    first commit

--- hotspot/hotspot/settings.py
+++ hotspot/hotspot/settings.py
@@ -11,10 +11,21 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+IS_LINUX = sys.platform.startswith('linux')
+
+
+class SQL:
+
+    NAME = 'hotspot'
+    USER: 'cone'
+    PASSWORD = 'Cone3.1415926' if IS_LINUX else '3.1415926'
+    HOST = '127.0.0.1' if IS_LINUX else '47.94.99.0'
+    PORT = '3306'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -81,11 +92,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hotspot',
-        'USER': 'cone',
-        'PASSWORD': '3.1415926',
-        'HOST': '47.94.99.0',
-        'PORT': '3306',
+        'NAME': SQL.NAME,
+        'USER': SQL.USER,
+        'PASSWORD': SQL.PASSWORD,
+        'HOST': SQL.HOST,
+        'PORT': SQL.PORT,
     }
 }
 
