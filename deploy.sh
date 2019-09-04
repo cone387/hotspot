34e5144d5f4486a748407790bebd1c254b29e25c
cone <1183008540@qq.com>
2019-09-04 17:14:01 +0800

    修复deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,18 @@ cd /root/user/hotspot
 
 ps -ef | grep manage.py |awk '{print $2}' | xargs kill -9
 
-type = 0     # 0, 采集， 1, 服务，2， 全部
+type=0     # 0, 采集， 1, 服务，2， 全部
 
-if [$type == 0]
+if [ $type == 0 ]
 then
   cd hotspot
   python3 manage.py makemigrations
   python3 manage.py migrate
   python3 manage.py runserver 0.0.0.0:80
-elif [$type == 1]
+elif [ $type == 1 ]
 then
   nohup python3 deploy_spider.py -P 2 -t 4 -I > website.spider.log 2>&1 &
-elif [$tpye == 2]
+elif [ $tpye == 2 ]
 then
   cd hotspot
   python3 manage.py makemigrations
@@ -23,4 +23,5 @@ then
   nohup python3 deploy_spider.py -P 2 -t 4 -I > website.spider.log 2>&1 &
 else
   echo "not type"
+fi
 echo "deploy"
