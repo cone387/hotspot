4478a9589958e3fe8967dcf4bce119006b9c7484
cone <1183008540@qq.com>
2019-09-05 16:07:45 +0800

    修复deploy.sh

--- hotspot/deploy_server.sh
+++ hotspot/deploy_server.sh
@@ -1,10 +1,10 @@
 ps -ef | grep manage.py |awk '{print $2}' | xargs kill -9
 
-if [ $1 == '0']
+if [ $1 == '0' ]
 then
   cd /root/user/hotspot/hotspot
 fi
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:80
+nohup python3 manage.py runserver 0.0.0.0:80 &
